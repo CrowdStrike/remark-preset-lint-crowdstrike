95babafabcf692cdb7769d18ecc80fc38dc80179
Kelly Selden <kellyselden@gmail.com>
2023-11-09 11:46:09 -0800

    fix: switch to string syntax

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 
 module.exports.plugins = [
-  require('remark-preset-lint-recommended'),
-  require('remark-preset-lint-consistent'),
-  // require('remark-lint-blank-lines-1-0-2'), buggy
-  // [require('remark-lint-code'), {
+  'remark-preset-lint-recommended',
+  'remark-preset-lint-consistent',
+  // 'remark-lint-blank-lines-1-0-2', // buggy
+  // ['remark-lint-code', {
   //   js: 'remark-lint-code-eslint',
   // }],
-  // require('remark-lint-no-empty-sections'),
-  require('remark-lint-heading-whitespace'),
-  require('remark-lint-no-url-trailing-slash'),
+  // 'remark-lint-no-empty-sections',
+  'remark-lint-heading-whitespace',
+  'remark-lint-no-url-trailing-slash',
   'remark-validate-links',
-  // require('lint-match-punctuation'), // doesn't work
+  // 'lint-match-punctuation', // doesn't work
 ];
